471fcecbc53c315fe403a7196945ada77f20bc3f
Alob <afonso.baixo@ua.pt>
2023-03-25 23:07:30 +0000

    Sign Up Fields Validation

--- website/static/javascript/main.js
+++ website/static/javascript/main.js
@@ -1,6 +1,38 @@
-let login = document.getElementById('login')
+let emailError = document.getElementById('email-error')
+let email = document.getElementById('email')
+let passwordError = document.getElementById('password1-error')
+let password = document.getElementById('password1')
+let confPass = document.getElementById('password2')
+let confPassErr = document.getElementById('password2-error')
 
-console.log(login);
-login.addEventListener('click', function(){
-    window.location.href = '/login';
+email.addEventListener("keyup", function(){
+    if(!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = "Please enter a valid email.";
+        email.style.borderColor = "red"
+    }
+    else{
+        email.style.borderColor = "green"
+        emailError.innerHTML = ""
+    }
+})
+
+password.addEventListener('keyup', function(){
+    if(password.value.length < 8 && password.value.length > 0){
+        passwordError.innerHTML = "Password must be over 7 characters.";
+        password.style.borderColor = "red";
+        confPass.style.borderColor = "red";
+    }
+    else{
+        passwordError.innerHTML = "";
+    }
+
+    if(password.value.length == 0){
+
+    }
+})
+confPass.addEventListener('keyup', function(){
+    if(password.value == confPass.value && password.value.length > 7){
+        password.style.borderColor = "green";
+        confPass.style.borderColor = "green";
+    }
 })
