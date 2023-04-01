a82719eb20ec471d2a20b2e341193f55c0ce7239
Alob <afonso.baixo@ua.pt>
2023-04-01 17:16:23 +0100

    display of containers

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = create_app() # create_app() in __init__.py
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = b'*51_.2S7H2F\n\xec]/'
-app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=1)
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=30)
 
 
 
