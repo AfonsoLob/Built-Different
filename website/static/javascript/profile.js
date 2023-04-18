a102c9406b8b024178f65aabb22e038e7a112a6a
Alob <afonso.baixo@ua.pt>
2023-04-18 13:22:07 +0100

    Profile Development

--- website/static/javascript/profile.js
+++ website/static/javascript/profile.js
@@ -2,16 +2,35 @@ let submit_btn = document.getElementById('get-weight');
 let navLinks = document.getElementsByClassName('nav-link');
 let linksArray = Array.from(navLinks);
 
-// submit_btn.addEventListener('click', function(){
-//     let weight = document.getElementById('weight').value;
-//     document.getElementById('graph').innerHTML = weight;
-// })
-
+submit_btn.addEventListener('click', function(){
+    let weight = document.getElementById('weight').value;
+    document.getElementById('graph').innerHTML = weight;
+})
 linksArray.forEach(link => {
-    link.addEventListener('click', function(){
-        if(!link.classList.contains('active') && link.id != 'settings'){
-            document.querySelector('.nav-link.active').classList.toggle('active');
-            link.classList.toggle('active');
-        }
-    })
+    if(link.id != 'settings'){ // link is not settings
+        link.addEventListener('click', function(){
+            if(!link.classList.contains('active')){
+                document.querySelector('.nav-link.active').classList.toggle('active');
+                link.classList.toggle('active');
+                let target = link.getAttribute('data-target');
+                document.querySelector('.content.is-selected').classList.toggle('is-selected');
+                let elTarget = document.getElementById(target);
+                elTarget.classList.toggle('is-selected');
+            }
+        })
+    }
+    else{ // link is settings
+        let settingsLinks = document.getElementsByClassName('dropdown-item');
+        let settingsArray = Array.from(settingsLinks);
+        settingsArray.forEach(link => {
+            link.addEventListener('click', function(){
+                document.querySelector('.nav-link.active').classList.toggle('active');
+                document.getElementById('settings').classList.toggle('active');
+                let target = link.getAttribute('data-target');
+                document.querySelector('.content.is-selected').classList.toggle('is-selected');
+                let elTarget = document.getElementById(target);
+                elTarget.classList.toggle('is-selected');
+            })
+        })
+    }
 });
